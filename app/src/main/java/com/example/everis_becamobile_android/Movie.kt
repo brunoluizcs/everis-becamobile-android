1887824da83c91772416153e90c268dce57036a2
andersonms92 <anderson.matos.santos@everis.com>
2021-03-29 17:18:44 -0300

    adicionando o recebimento de id de gênero de filme

--- app/src/main/java/com/example/everis_becamobile_android/Movie.kt
+++ app/src/main/java/com/example/everis_becamobile_android/Movie.kt
@@ -4,6 +4,7 @@ import com.google.gson.annotations.SerializedName
 
 data class Movie(
     @SerializedName("id") val id: Long,
+    @SerializedName("genre_ids") val genre_id: Int,
     @SerializedName("title") val title: String,
     @SerializedName("overview") val overview: String,
     @SerializedName("poster_path") val poster_path: String,
