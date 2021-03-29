420c928d9a14ceaad590f1bf015ace1cd329fc1b
Rafael Heck <rafaelheck7@gmail.com>
2021-03-29 17:45:49 -0300

    Corrigindo Url Base

--- MoviesApp/app/src/main/java/com/example/moviesapp/api/MovieRestApiTask.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/api/MovieRestApiTask.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MovieRestApiTask {
 
     companion object{
-        const val BASE_URL = "https://api.themoviedb.org/"
+        const val BASE_URL = "https://api.themoviedb.org/3/"
     }
 
     private fun movieProvider(): Retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
