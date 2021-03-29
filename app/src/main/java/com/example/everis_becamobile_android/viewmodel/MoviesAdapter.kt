60a250318b0bff84391bf7554a351716d9b5d989
andersonms92 <anderson.matos.santos@everis.com>
2021-03-29 13:20:26 -0300

    criando adapter e inserido imagem no layout

---
+++ app/src/main/java/com/example/everis_becamobile_android/viewmodel/MoviesAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.everis_becamobile_android.viewmodel
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.example.everis_becamobile_android.R
+import com.example.everis_becamobile_android.model.Movie
+
+class MoviesAdapter (
+    private var movies: List<Movie>
+) : RecyclerView.Adapter<MoviesAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MovieViewHolder {
+        val view = LayoutInflater
+            .from(parent.context)
+            .inflate(R.layout.item_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) = holder.bind(movies[position])
+
+    fun updateMovies(movies: List<Movie>) {
+        this.movies = movies
+        notifyDataSetChanged()
+    }
+
+    inner class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private val image: ImageView = itemView.findViewById(R.id.item_movie_image)
+
+        fun bind(movie: Movie) {
+            Glide.with(itemView)
+                .load("https://image.tmdb.org/t/p/w342${movie.poster_path}")
+                .transform(CenterCrop())
+                .into(image)
+        }
+    }
+}
