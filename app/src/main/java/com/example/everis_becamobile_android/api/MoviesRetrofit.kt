f98fafe23bae02e9f981496cb8cfe574bbe50345
andersonms92 <anderson.matos.santos@everis.com>
2021-03-29 12:30:45 -0300

    criando função getTrendingMovies

--- app/src/main/java/com/example/everis_becamobile_android/api/MoviesRetrofit.kt
+++ app/src/main/java/com/example/everis_becamobile_android/api/MoviesRetrofit.kt
@@ -1,5 +1,11 @@
 package com.example.everis_becamobile_android.api
 
+import android.util.Log
+import com.example.everis_becamobile_android.model.Movie
+import com.example.everis_becamobile_android.model.MoviesResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -13,5 +19,29 @@ object MoviesRetrofit {
             .build()
 
         api = retrofit.create(MovieApi::class.java)
+    }
+
+    fun getTrendingMovies(page: Int = 1) {
+        api.getTrendingMovies(page = page)
+            .enqueue(object : Callback<MoviesResponse> {
+                override fun onResponse(
+                    call: Call<MoviesResponse>,
+                    response: Response<MoviesResponse>
+                ) {
+                   if (response.isSuccessful) {
+                       val responseBody = response.body()
+
+                       if (responseBody != null) {
+                           Log.d("Repository", "Movies: ${responseBody.movies_results}")
+                       } else {
+                           Log.d("Repository", "Failed to get response")
+                       }
+                   }
+                }
+
+                override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
+                    Log.e("Repository", "onFailure", t)
+                }
+            })
     }
 }
