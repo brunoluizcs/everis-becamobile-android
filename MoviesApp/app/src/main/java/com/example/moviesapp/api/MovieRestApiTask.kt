915f459ca9e08c60219b5ef9fa5d90c65315dbd6
Rafael Heck <rafaelheck7@gmail.com>
2021-03-29 15:42:59 -0300

    Implementando metodo de request para url base utilizando Retrofit

--- MoviesApp/app/src/main/java/com/example/moviesapp/api/MovieRestApiTask.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/api/MovieRestApiTask.kt
@@ -1,4 +1,15 @@
 package com.example.moviesapp.api
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 class MovieRestApiTask {
+
+    companion object{
+        const val BASE_URL = "https://api.themoviedb.org/"
+    }
+
+    private fun movieProvider(): Retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
+
+    fun retrofitApi(): ApiMoviesDB = movieProvider().create(ApiMoviesDB::class.java)
 }
