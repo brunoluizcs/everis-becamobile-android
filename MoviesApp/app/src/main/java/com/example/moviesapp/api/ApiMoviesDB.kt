15c8bcaa06299722784b8d9966b60ed022948da8
Rafael Heck <rafaelheck7@gmail.com>
2021-03-29 17:34:00 -0300

    Alterando retorno para MoviesResult, onde é retornado a lista de filmes Trending

--- MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
@@ -1,14 +1,17 @@
 package com.example.moviesapp.api
 
 import com.example.moviesapp.model.Movie
+import com.example.moviesapp.model.MoviesResult
+import org.json.JSONObject
+import retrofit2.Call
 import retrofit2.http.GET
 
 interface ApiMoviesDB {
 
     companion object{
-        const val TRENDING_PATH = "3/trending/movies/week?api_key=6014c47f4de9e039fe2ddbdc70084887"
+        const val TRENDING_PATH = "trending/movies/week?api_key=6014c47f4de9e039fe2ddbdc70084887"
     }
 
     @GET(TRENDING_PATH)
-    fun getWeekTrendingMovies(): List<Movie>
+    fun getWeekTrendingMovies(): Call<MoviesResult>
 }
