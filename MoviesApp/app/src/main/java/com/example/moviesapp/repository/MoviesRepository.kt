8000ea10098d2f2b40b2b9e20d5ddb10596507e3
Rafael Heck <rafaelheck7@gmail.com>
2021-03-29 17:40:07 -0300

    Criando repositório de filmes, onde será formada a lista com todos os filmes (Ainda sem tratação de exceptions)

--- MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
@@ -1,4 +1,18 @@
 package com.example.moviesapp.repository
 
-class MoviesRepository(){
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.moviesapp.api.MovieRestApiTask
+import com.example.moviesapp.model.Movie
+import com.example.moviesapp.model.MoviesResult
+
+class MoviesRepository(private val movieRestApiTask: MovieRestApiTask){
+    private val movieList = arrayListOf<Movie>()
+
+    fun getTrendingMovies(): List<Movie> {
+        val request = movieRestApiTask.retrofitApi().getWeekTrendingMovies().execute()
+
+        return request.body()!!.results
+    }
+
 }
