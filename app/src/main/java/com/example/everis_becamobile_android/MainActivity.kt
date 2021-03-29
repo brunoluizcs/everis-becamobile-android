6bfcca757ad027614c1116410193bd25884a8727
andersonms92 <anderson.matos.santos@everis.com>
2021-03-29 13:29:13 -0300

    instancionando RecyclerView e Adapter

--- app/src/main/java/com/example/everis_becamobile_android/MainActivity.kt
+++ app/src/main/java/com/example/everis_becamobile_android/MainActivity.kt
@@ -4,13 +4,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.everis_becamobile_android.api.MoviesRetrofit
 import com.example.everis_becamobile_android.model.Movie
+import com.example.everis_becamobile_android.viewmodel.MoviesAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var trendingMovies: RecyclerView
+    private lateinit var trendingMoviesAdapter: MoviesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        trendingMovies = findViewById(R.id.trending_movies)
+        trendingMovies.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        trendingMoviesAdapter = MoviesAdapter(listOf())
+        trendingMovies.adapter = trendingMoviesAdapter
 
         MoviesRetrofit.getTrendingMovies(
             onSuccess = ::onTrendingMoviesFetched,
@@ -19,7 +35,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onTrendingMoviesFetched(movies: List<Movie>) {
-        Log.d("MainActivity", "Movies: $movies")
+        trendingMoviesAdapter.updateMovies(movies)
     }
 
     private fun onError() {
