0d0b77f4b2320bd76c2281c0a5d0f26f73fdd00e
Rafael Heck <rafaelheck7@gmail.com>
2021-03-29 16:00:58 -0300

    Criando classe MoviesRepository

---
+++ MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
@@ -0,0 +1,4 @@
+package com.example.moviesapp.repository
+
+class MoviesRepository(){
+}
--- MoviesApp/app/src/main/java/com/example/moviesapp/view/MainActivity.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.moviesapp
+package com.example.moviesapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.moviesapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
