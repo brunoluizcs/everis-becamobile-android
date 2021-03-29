5c85d4565366cf8f5fb39dffbdfce0cd3d1c282b
Rafael Heck <rafaelheck7@gmail.com>
2021-03-29 15:45:26 -0300

    Implementando metodo GET para url de filmes tendencia da semana

--- MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
@@ -1,5 +1,14 @@
 package com.example.moviesapp.api
 
+import com.example.moviesapp.model.Movie
+import retrofit2.http.GET
+
 interface ApiMoviesDB {
-    
+
+    companion object{
+        const val TRENDING_PATH = "3/trending/movies/week?api_key=6014c47f4de9e039fe2ddbdc70084887"
+    }
+
+    @GET(TRENDING_PATH)
+    fun getWeekTrendingMovies(): List<Movie>
 }
