9575a63acd457f7cebd620bf24b3b7f7ae90ce85
Rafael Heck <rafaelheck7@gmail.com>
2021-03-30 16:52:43 -0300

    Criando metodos para popular a lista de filmes e exibir na main activity

--- MoviesApp/app/src/main/java/com/example/moviesapp/view/MainActivity.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/view/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.moviesapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.moviesapp.MovieListViewModel.MovieListViewModel
 import com.example.moviesapp.R
+import com.example.moviesapp.api.MovieRestApiTask
+import com.example.moviesapp.model.Movie
+import com.example.moviesapp.repository.MoviesAdapter
+import com.example.moviesapp.repository.MoviesRepository
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var movieListViewModel: MovieListViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        movieListViewModel = ViewModelProvider.NewInstanceFactory().create(MovieListViewModel::class.java)
+        movieListViewModel.init()
+        initObserver()
+    }
+
+    private fun initObserver(){
+        movieListViewModel.moviesList.observe(this, Observer {
+            populateList(it)
+        })
+    }
+
+    private fun populateList(list: List<Movie>){
+        trendingMovies.apply{
+            hasFixedSize()
+            adapter = MoviesAdapter(list)
+        }
     }
 }
