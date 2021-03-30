dd7af1279e4150f573514deaf3c5a07ce140049a
Rafael Heck <rafaelheck7@gmail.com>
2021-03-30 16:44:14 -0300

    Adicionando função para retornar o valor da requisição aos detalhes do filme (Ainda sem tratamento de erros e excessões)

--- MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
@@ -7,11 +7,15 @@ import com.example.moviesapp.model.Movie
 import com.example.moviesapp.model.MoviesResult
 
 class MoviesRepository(private val movieRestApiTask: MovieRestApiTask){
-    private val movieList = arrayListOf<Movie>()
 
     fun getTrendingMovies(): List<Movie> {
         val request = movieRestApiTask.retrofitApi().getWeekTrendingMovies().execute()
-        return request.body()!!.results
+        return request.body()?.results!!
+    }
+
+    fun getMovieDetails(movieId: Int): Movie? {
+        val request = movieRestApiTask.retrofitApi().getMovieDetails(movieId).execute()
+        return request.body()
     }
 
 }
