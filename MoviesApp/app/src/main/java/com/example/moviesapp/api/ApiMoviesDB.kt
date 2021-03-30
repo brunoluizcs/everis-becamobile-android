3ef67ae0cc0ece3dac0742c02c316ddd227294b2
Rafael Heck <rafaelheck7@gmail.com>
2021-03-30 09:41:23 -0300

    Corrigindo caminho da chamada de movies para movie

--- MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
@@ -9,7 +9,7 @@ import retrofit2.http.GET
 interface ApiMoviesDB {
 
     companion object{
-        const val TRENDING_PATH = "trending/movies/week?api_key=6014c47f4de9e039fe2ddbdc70084887"
+        const val TRENDING_PATH = "trending/movie/week?api_key=6014c47f4de9e039fe2ddbdc70084887"
     }
 
     @GET(TRENDING_PATH)
