84742c118323c565aa6a6d4777445159c4dc64be
Rafael Heck <rafaelheck7@gmail.com>
2021-03-30 14:18:39 -0300

    Adicionando request para detalhes do filme

--- MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/api/ApiMoviesDB.kt
@@ -4,14 +4,24 @@ import com.example.moviesapp.model.Movie
 import com.example.moviesapp.model.MoviesResult
 import org.json.JSONObject
 import retrofit2.Call
+import retrofit2.Callback
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiMoviesDB {
 
     companion object{
-        const val TRENDING_PATH = "trending/movie/week?api_key=6014c47f4de9e039fe2ddbdc70084887"
+        const val API_KEY = "?api_key=6014c47f4de9e039fe2ddbdc70084887"
+        const val TRENDING_PATH = "trending/movie/week$API_KEY"
+        const val MOVIE_DETAILS_PATH = "movie/"
     }
 
     @GET(TRENDING_PATH)
     fun getWeekTrendingMovies(): Call<MoviesResult>
+
+    @GET("$MOVIE_DETAILS_PATH{id}$API_KEY")
+    fun getMovieDetails(@Path("id") id: Int): Call<Movie>
+
+
 }
