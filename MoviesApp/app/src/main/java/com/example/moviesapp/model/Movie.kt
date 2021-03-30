4007ab28b7c4b88a0912c1f64709d63d33b28250
Rafael Heck <rafaelheck7@gmail.com>
2021-03-30 10:55:15 -0300

    Ajustando data class Movie, serializando as tags do json

--- MoviesApp/app/src/main/java/com/example/moviesapp/model/Movie.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/model/Movie.kt
@@ -1,15 +1,26 @@
 package com.example.moviesapp.model
 
+import com.google.gson.annotations.SerializedName
+
 data class Movie (
     val id: Int,
     val adult: Boolean,
     val title: String,
+    @SerializedName("original_title")
     val originalTitle: String,
+    @SerializedName("vote_count")
     val voteCount: Int,
+    @SerializedName("vote_average")
     val voteAverage: Double,
+    @SerializedName("popularity")
     val popularity: Double,
     val overview: String,
+    @SerializedName("release_date")
     val releaseDate: String,
+    @SerializedName("backdrop_path")
     val backdropPath: String,
-    val posterPath: String
+    @SerializedName("poster_path")
+    val posterPath: String,
+    @SerializedName("media_type")
+    val mediaType: String
 )
