7c1d27c4c6b6c7b8c370c68777fa0c1316f812a0
Rafael Heck <rafaelheck7@gmail.com>
2021-03-30 15:30:52 -0300

    Criando classe responsavel por criar a lista de filmes e utilizar o MoviesRepository para popular esta lista através das chamadas para Api

---
+++ MoviesApp/app/src/main/java/com/example/moviesapp/MovieListViewModel/MovieListViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.moviesapp.MovieListViewModel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.moviesapp.api.MovieRestApiTask
+import com.example.moviesapp.model.Movie
+import com.example.moviesapp.repository.MoviesRepository
+import java.lang.Exception
+
+class MovieListViewModel: ViewModel() {
+
+    private val movieRestApiTask = MovieRestApiTask()
+    private val moviesRepository = MoviesRepository(movieRestApiTask)
+
+    private var _moviesList = MutableLiveData<List<Movie>>()
+    val moviesList: LiveData<List<Movie>>
+        get() = _moviesList
+
+    fun init(){
+        getTrendingMovies()
+    }
+
+    private fun getTrendingMovies(){
+        Thread{
+            try{
+                _moviesList.postValue(moviesRepository.getTrendingMovies())
+            }catch (exception: Exception){
+                Log.d("MovieListViewModel", exception.message.toString())
+            }
+        }
+    }
+}
