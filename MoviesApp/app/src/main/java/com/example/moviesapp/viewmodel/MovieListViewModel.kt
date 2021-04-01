e3acc9c73069887950be10b485db6a7c6cec6489
Rafael Heck <rafaelheck7@gmail.com>
2021-04-01 12:14:06 -0300

    Criando metodos que serão invocados durante requisição a Api

--- MoviesApp/app/src/main/java/com/example/moviesapp/viewmodel/MovieListViewModel.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/viewmodel/MovieListViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.moviesapp.viewmodel
 
+import android.content.Context
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -23,12 +25,18 @@ class MovieListViewModel: ViewModel() {
     }
 
     private fun getTrendingMovies(){
-        Thread{
-            try{
-                _moviesList.postValue(moviesRepository.getTrendingMovies())
-            }catch (exception: Exception){
-                Log.d("MovieListViewModel", exception.message.toString())
-            }
-        }.start()
+        _moviesList.postValue(moviesRepository.getTrendingMovies())
+    }
+
+    private fun onRequestSuccess(list: List<Movie>){
+        _moviesList.postValue(list)
+    }
+
+    private fun onRequestError(context: Context, code: Int?){
+        if(code != null){
+            Toast.makeText(context, "Error $code", Toast.LENGTH_SHORT)
+        }else{
+            Toast.makeText(context, "Unable to load movie details", Toast.LENGTH_SHORT)
+        }
     }
 }
