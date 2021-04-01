748785c1aada297ff9bf045d117c77044915c3cf
Rafael Heck <rafaelheck7@gmail.com>
2021-04-01 12:28:48 -0300

    Criando tratativa para falha na requisição e retorno com erros

--- MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.widget.Toast
 import com.example.moviesapp.api.MovieRestApiTask
 import com.example.moviesapp.model.Movie
+import com.example.moviesapp.model.MoviesResult
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,9 +15,28 @@ class MoviesRepository(private val movieRestApiTask: MovieRestApiTask){
         private const val REQUEST_SUCCESS = 200
     }
 
-    fun getTrendingMovies(): List<Movie> {
-        val request = movieRestApiTask.retrofitApi().getWeekTrendingMovies().execute()
-        return request.body()?.results!!
+    fun getTrendingMovies(context: Context, onSuccess: (List<Movie>) -> Unit, onError: (Context, Int?) -> Unit) {
+        val request = movieRestApiTask.retrofitApi().getWeekTrendingMovies()
+
+        request.enqueue(object: Callback<MoviesResult>{
+            override fun onResponse(call: Call<MoviesResult>, response: Response<MoviesResult>) {
+                val responseCode = response.code()
+
+                if(responseCode == REQUEST_SUCCESS){
+                    val moviesList = response.body()!!.results
+                    onSuccess.invoke(moviesList)
+                }else{
+                    onError.invoke(context, responseCode)
+                }
+            }
+
+            override fun onFailure(call: Call<MoviesResult>, t: Throwable) {
+                Toast.makeText(context, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+
     }
 
     fun getMovieDetails(movieId: Int, context: Context, onSuccess: (Movie) -> Unit, onError: (Context, Int?) -> Unit) {
