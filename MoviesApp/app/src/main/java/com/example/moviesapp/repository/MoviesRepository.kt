89013c7ef87b498202a1304c74e05fc42722acf1
Rafael Heck <rafaelheck7@gmail.com>
2021-03-31 21:47:35 -0300

    removendo importações não utilizadas

--- MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/repository/MoviesRepository.kt
@@ -1,10 +1,7 @@
 package com.example.moviesapp.repository
 
-import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import com.example.moviesapp.api.MovieRestApiTask
 import com.example.moviesapp.model.Movie
-import com.example.moviesapp.model.MoviesResult
 
 class MoviesRepository(private val movieRestApiTask: MovieRestApiTask){
 
