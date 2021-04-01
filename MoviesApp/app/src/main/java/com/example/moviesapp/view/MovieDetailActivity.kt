6602780f2da2d5f74a059dd2c7f5eb0484bd0d88
Rafael Heck <rafaelheck7@gmail.com>
2021-03-31 21:50:57 -0300

    Adicionando toolbar com botão de retorno a main activity, colocando ProgressBar enquanto imagem não for carregada e exibindo titulo, nota e overview do filme selecionado

--- MoviesApp/app/src/main/java/com/example/moviesapp/view/MovieDetailActivity.kt
+++ MoviesApp/app/src/main/java/com/example/moviesapp/view/MovieDetailActivity.kt
@@ -1,7 +1,7 @@
 package com.example.moviesapp.view
 
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.lifecycle.Observer
@@ -10,10 +10,10 @@ import com.example.moviesapp.R
 import com.example.moviesapp.model.Movie
 import com.example.moviesapp.repository.MoviesAdapter
 import com.example.moviesapp.viewmodel.MovieDetailsViewModel
-import com.example.moviesapp.viewmodel.MovieListViewModel
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.movie_details.*
-import kotlinx.android.synthetic.main.movie_item_layout.view.*
+import kotlinx.android.synthetic.main.movie_details.progressBar
+import java.lang.Exception
 
 class MovieDetailActivity : AppCompatActivity(){
 
@@ -25,19 +25,51 @@ class MovieDetailActivity : AppCompatActivity(){
         movieDetailsViewModel = ViewModelProvider.NewInstanceFactory().create(MovieDetailsViewModel::class.java)
         movieDetailsViewModel.init(intent.getIntExtra("id", -1))
 
+        initToolbar()
         initObserver()
+        loadingVisibility(true)
     }
 
     private fun initObserver(){
         movieDetailsViewModel.movieDetails.observe(this, Observer {
-           setupMovie(it)
+            setupMovie(it)
         })
     }
 
     private fun setupMovie(movie: Movie){
-        moviePoster.apply {
-            Picasso.get().load("${MoviesAdapter.IMG_BASE_URL}${movie.posterPath}").placeholder(R.drawable.ic_image_placeholder).into(moviePoster)
+        movieDetailsPoster.apply {
+            loadImage(movieDetailsPoster, movie)
         }
+        movieDetailsRate.text = movie.voteAverage.toString()
+        movieDetailsTitle.text = movie.title
+        movieDetailsOverview.text = movie.overview
+    }
+
+    private fun loadImage(imageView: AppCompatImageView, movie: Movie){
+        Picasso.get().load("${MoviesAdapter.IMG_BASE_URL}${movie.posterPath}").placeholder(R.drawable.ic_image_placeholder).into(imageView, object: com.squareup.picasso.Callback {
+            override fun onSuccess() {
+                loadingVisibility(false)
+            }
+
+            override fun onError(e: Exception?) {
+
+            }
+        })
+    }
+
+    private fun loadingVisibility(isLoading: Boolean){
+        progressBar.visibility = if (isLoading) View.VISIBLE else View.GONE
+    }
+
+    private fun initToolbar(){
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean{
+        finish()
+        return true
     }
 
 }
