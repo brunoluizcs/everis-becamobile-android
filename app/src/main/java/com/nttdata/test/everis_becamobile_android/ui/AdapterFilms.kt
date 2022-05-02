40373953e100584ddbf0b1e461ec51b4f6f9cbce
tpaivafe <theylonfeitosa@gmail.com>
2022-05-02 16:31:24 -0300

    Setup MainActivity UI

---
+++ app/src/main/java/com/nttdata/test/everis_becamobile_android/ui/AdapterFilms.kt
@@ -0,0 +1,61 @@
+package com.nttdata.test.everis_becamobile_android.ui
+
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.nttdata.test.everis_becamobile_android.databinding.ItemListFilmsBinding
+import com.nttdata.test.everis_becamobile_android.model.JsonFilms
+import com.nttdata.test.everis_becamobile_android.model.Result
+
+class AdapterFilms: ListAdapter<Result,AdapterFilms.ViewHolder>(DIFF_CALLBACK) {
+    var onClickListener:((filmId:Int)->Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemListFilmsBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding,onClickListener)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class ViewHolder(
+        private val binding: ItemListFilmsBinding,
+        private val onClickListener: ((filmId:Int)->Unit)? = null
+    ):RecyclerView.ViewHolder(binding.root){
+        fun bind(film:Result){
+            binding.ratingBar.rating = film.vote_average.toFloat()/2
+            binding.tvItemRating.text = film.vote_average.toString()
+            binding.tvItemTitle.text = film.title
+            if (film.adult){
+                binding.tvItemGenere.text = "Adulto"
+            }else{
+                binding.tvItemGenere.text = "Livre"
+            }
+            binding.tvItemDescription.text = film.overview
+
+            binding.cvItem.setOnClickListener {
+                onClickListener?.invoke(film.id)
+            }
+
+        }
+    }
+
+    companion object{
+        private val DIFF_CALLBACK = object : DiffUtil.ItemCallback<Result>(){
+            override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/nttdata/test/everis_becamobile_android/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.nttdata.test.everis_becamobile_android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nttdata.test.everis_becamobile_android.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
