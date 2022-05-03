01c0d8e280d3a23b3db172a65173de8b0d79c87a
hjesusmo <heriveltojunior1@gmail.com>
2022-05-03 10:34:02 -0300

    Initial commit to Pratice Test

---
+++ PraticeTest/app/src/main/java/com/example/praticetest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.praticetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
